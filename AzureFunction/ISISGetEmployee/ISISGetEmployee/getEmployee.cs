3f65b4a6d76097cf695665852a97d619a0f2114a
Rajal Patel <rajal.personal@gmail.com>
2021-10-04 10:05:56 -0500

    Azure Function

---
+++ AzureFunction/ISISGetEmployee/ISISGetEmployee/getEmployee.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ISISGetEmployee
+{
+    public static class getEmployee
+    {
+        private class Employee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string CodeName { get; set; }
+            public string Position { get; set; }
+            public string Status { get; set; }
+            public double WeeklyPay { get; set; }
+            public Agency Agency { get; set; }
+            private class Agency
+            {
+                public string Name { get; set; }
+                public string Address { get; set; }
+                public string Phone { get; set; }
+            }
+            public Employee(string strFirstName, string strLastName, string strCodeName, string strPosition, string strStatus, double dblPayRate, double dblHours, string strAgency, Agency agSpyAgency)
+            {
+                FirstName = strFirstName;
+                LastName = strLastName;
+                CodeName = strCodeName;
+                Position = strPosition;
+                Status = strStatus;
+                WeeklyPay = dblPayRate * dblHours;
+                Agency = agSpyAgency;
+            }
+
+        }
+
+        [FunctionName("getEmployee")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string strCodeName = req.Query["CodeName"];
+            string strAgency = req.Query["Agency"];
+            log.LogInformation("HTTP trigger on getEmployee processed a request for: " + strCodeName);
+
+            string 
+
+            Employee Archer = new Employee("Sterling", "Archer", "Duchess", "Field Agent", "Active", 23.75, 18.50, "ISIS");
+            Employee Lana = new Employee("Lana", "Kane", "Truckasaurus", "Field Agent", "Active", 21.50, 23.50, "ISIS");
+            Employee Pam = new Employee("Pam", "Poovey", "Snowball", "Human Resource Director", "Active", 49.00, 12, "ISIS");
+            Employee Barry = new Employee("Barry", "Cyborg", "Duchess", "Field Agent", "Active", 23.75, 18.50, "CIA");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            Employee[] arrEmployees = new Employee[] { Archer, Lana, Pam, Barry };
+
+            Employee[] arrFoundEmployees = new Employee[] { };
+            foreach(Employee empCurrent in arrEmployees)
+            {
+                if(strCodeName == empCurrent.CodeName)
+                {
+                    arrFoundEmployees.
+                }
+            }
+            return new OkObjectResult("Employee Not Found");
+        }
+    }
+}
---
+++ Profile/js/app.js
@@ -0,0 +1,13 @@
+var arrProfileData;
+$.getJSON("https://www.swollenhippo.com/getProfileDetailsByAPIKey.php?APIKey=DuffManSays,Phrasing!&Codename=Duchess", function(result){
+    console.log(result)
+    arrProfileData = result;
+    fillProfile(arrProfileData[0]);
+})
+
+function fillProfile(Employee){
+    $('#txtEmployeeName').text(Employee.FirstName + ' ' + Employee.LastName)
+    $('#txtCodename').text(Employee.CodeName)
+    $('#txtEmployeeJob').text(Employee.Job)
+
+}
